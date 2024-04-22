b8025974cf9851743aaa77a06aff5f5919e45ae3
kia <labeast0021@gmail.com>
2024-04-21 21:15:17 -0700

    added new google auth callback link to check

--- config/database.js
+++ config/database.js
@@ -18,6 +18,62 @@
 //   // useCreateIndex: true,
 //   // useUnifiedTopology: true
 
+// ============================== SECOND VERSION THAT SEEMS TO BE WORKING ==============================
+// var mongoose = require('mongoose');
+
+// // Connection options for best practice and handling reconnections
+// const options = {
+//     serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds while trying to connect
+//     socketTimeoutMS: 45000,        // Close sockets after 45 seconds of inactivity
+//     family: 4                      // Use IPv4, skip trying IPv6
+// };
+
+// // Connecting to MongoDB
+// mongoose.connect(process.env.DATABASE_URL, options).catch(err => {
+//     console.error('Initial MongoDB connection error:', err.message);
+// });
+
+// var db = mongoose.connection;
+
+// // Successful connection
+// db.on('connected', () => {
+//     console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+// });
+
+// // Connection throws an error
+// db.on('error', err => {
+//     console.error(`MongoDB connection error: ${err.message}`);
+//     // Optionally, you could implement a reconnection strategy here
+//     mongoose.disconnect();
+// });
+
+// // Connection is disconnected
+// db.on('disconnected', () => {
+//     console.log('MongoDB disconnected!');
+//     // Attempt to reconnect every 5 seconds
+//     setTimeout(() => mongoose.connect(process.env.DATABASE_URL, options), 5000);
+// });
+
+// // Close the Mongoose connection when the app is terminated
+// process.on('SIGINT', () => {
+//     db.close(() => {
+//         console.log('Mongoose connection disconnected due to app termination');
+//         process.exit(0);
+//     });
+// });
+
+// // Optional: Log when the connection is reconnected
+// db.on('reconnected', () => {
+//     console.log('MongoDB reconnected!');
+// });
+
+// // Optional: Log when reconnecting
+// db.on('reconnecting', () => {
+//     console.log('MongoDB reconnecting...');
+// });
+
+
+
 const mongoose = require('mongoose');
 const fs = require('fs');
 const ca = fs.readFileSync('path/to/ca.pem');
